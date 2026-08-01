0e7729f165d0ec7131b65b8e5314de785501b3b4
Mandasujeevana <sujeevanamanda@gmail.com>
2026-08-01 20:20:00 +0530

    changed the location of webhooks file

---
+++ backend/src/webhooks/clerk.webhook.js
@@ -0,0 +1,57 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+import fs from "fs";
+import path from "path";
+
+import { clerkMiddleware } from '@clerk/express'
+import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
+
+import clerkWebhook from "./webhooks/clerk.webhook.js";
+
+const app = express();
+
+const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const publicDir = path.join(process.cwd(), "public");
+
+app.use("/api/webhook/clerk",express.raw({type:"application/json"}),clerkWebhook);
+
+app.use(express.json())
+app.use(cors({ origin: FRONTEND_URL, credentials: true}));
+app.use(clerkMiddleware())
+
+
+app.get("/health", (req, res) =>{
+  res.status(200).json({ok: true});
+});
+
+if(fs.existsSync(publicDir)){
+
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}",(req,res,next) => {
+    res.sendFile(path.join(publicDir,"index.html"), (err) => next(err));
+  });
+}
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server is up and running on PORT: ${PORT}`);
+    });
+   if(process.env.NODE_ENV === "production") {
+    job.start();
+
+   } 
+  } catch (err) {
+    console.error("Failed to start server:", err);
+  }
+};
+
+startServer();
