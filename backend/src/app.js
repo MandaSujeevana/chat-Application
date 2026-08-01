ec847c1dc5aef79c0fbe50657d49c16b0e855772
Mandasujeevana <sujeevanamanda@gmail.com>
2026-08-01 20:06:56 +0530

    name change

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ import { clerkMiddleware } from '@clerk/express'
 import { connectDB } from "./lib/db.js";
 import job from "./lib/cron.js";
 
-import clerkWebhook from "./webhook/clerk.webhook.js";
+import clerkWebhook from "./webhooks/clerk.webhooks.js";
 
 const app = express();
 
