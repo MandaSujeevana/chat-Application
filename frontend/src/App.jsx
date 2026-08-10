f9241302a513d62aead2db3affae0f79459f85f1
Manda Sujeevana <sujeevanamanda@gmail.com>
2026-08-10 23:26:53 +0530

    -Updated main.jsx to include BrowserRouter for routing. -Created AuthPage and ChatPage components for future routing. -Removed vite.svg asset as it is no longer needed. -Updated index.css to integrate tailwind CSS and HeroUI styles. -Configured Vite to use Tailwind CSS plugin.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,35 @@
-import './App.css'
 import { useState } from 'react';
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
 import { useSyncUser } from './hooks/useSyncUser';
+import { WallpaperProvider } from "./context/WallpaperContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import { Navigate, Route, Routes } from "react-router";
+import ChatPage from "./pages/ChatPage.jsx";
+import AuthPage from './pages/AuthPage.jsx';
+import { useAuth } from "@clerk/react";
 
 
 function App() {
+  const { isSignedIn, isLoaded } = useAuth();
+  //todo: make this a better component
+  if (!isLoaded) return <p>loading...</p>;
+
   useSyncUser();
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1>MY APP</h1>
+    <ThemeProvider>
+     <WallpaperProvider>
+       <Routes>
+          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
+          <Route
+            path="/auth"
+            element={!isSignedIn ? <AuthPage /> : <Navigate to={"/chat"} replace />}
+          />
 
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal"/>
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </div>
+       </Routes>
+      
+     </WallpaperProvider>
+    </ThemeProvider>
   );
 }
 
---
+++ frontend/src/pages/AuthPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Authpage() {
+  return (
+    <div>Authpage</div>
+  )
+}
+
+export default Authpage
